41b48fca38e9d52b1273454990083891c50538af
Johan Smolinski <github.js@crypt.se>
2017-06-29 08:58:37 +0000

    ncurses play

--- src/ui/cli/demo.c
+++ src/ui/cli/demo.c
@@ -4,36 +4,46 @@
 #define SCREEN_WIDTH 32
 #define SCREEN_HEIGHT 24
 
-void track_print(WINDOW *track) {
+void track_print(WINDOW *track, int cursor_y, int cursor_x) {
     for (int i=0; i<16; i++) {
         mvwprintw(track, i, 0, "%x---", i);
     }
-    wmove(track, 0, 1);
+    wmove(track, cursor_y, cursor_x);
     wrefresh(track);
 }
 
 int main(int argc, char **argv) {
     int xmax, ymax;
-    int key_pressed = 0, old_key_pressed = 0;
+    int key_pressed = 0;
+    int cursor_y = 0, cursor_x = 2;
+    unsigned int delay = 20000;
 
     WINDOW *track;
 
 
     initscr();
     noecho();
+    cbreak();
     curs_set(TRUE);
+    keypad(stdscr, TRUE);
 
     track = newwin(16, 10, 0, 0);
-    nodelay(track, TRUE);
+    nodelay(stdscr, TRUE);
     getmaxyx(stdscr, ymax, xmax);
 
     while(1) {
-        track_print(track);
+        track_print(track, cursor_y, cursor_x);
 
-        key_pressed = 
-
+        key_pressed = getch();
+        switch(key_pressed) {
+          case 'j': cursor_y++; break;
+          case 'k': cursor_y--; break;
+          case 'h': cursor_x--; break;
+          case 'l': cursor_x++; break;
+        }
+        
         getmaxyx(stdscr, ymax, xmax);
-        usleep(20000);
+        usleep(delay);
     }
     endwin();
 
